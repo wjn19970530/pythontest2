3f3b5fcec1a31b95ef133c73c15c65cd3b7611ad
wjn19970530 <784628015@qq.com>
2020-08-23 23:51:38 +0800

    test

--- APIdemo/run.py
+++ APIdemo/run.py
@@ -2,6 +2,7 @@ from httprunner.api import HttpRunner
 from httprunner.report import gen_html_report
 
 runner = HttpRunner()
-# summary = runner.run("api/account/GET_verify-code.yml")
-summary = runner.run("testcases/verify_code_login.yml",dot_env_path="conf/.env")
+summary = runner.run("testcase/verify_code_login.yml",dot_env_path="conf/.env")
+# summary = runner.run("/api/account/GET_verify-code.yml",dot_env_path="conf/.env")
 gen_html_report(summary)
+
