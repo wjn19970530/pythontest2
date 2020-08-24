97aae1644313a3f140ac3d39346ad77d513fe91d
wjn19970530 <784628015@qq.com>
2020-08-24 18:23:48 +0800

    test

--- APIdemo/run.py
+++ APIdemo/run.py
@@ -3,6 +3,6 @@ from httprunner.report import gen_html_report
 
 runner = HttpRunner()
 # summary = runner.run("testcases/verify_code_login.yml",dot_env_path="conf/.env")
-summary = runner.run("api/mall/back-end/GET_car-items.yml",dot_env_path="conf/.env")
+summary = runner.run("api/mall/back-end/GET_current.yml",dot_env_path="conf/.env")
 gen_html_report(summary)
 
