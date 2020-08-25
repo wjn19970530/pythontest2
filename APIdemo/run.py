f38f36753f2bf615def0086c0fe7704dc83fede3
wjn19970530 <784628015@qq.com>
2020-08-25 16:23:59 +0800

    成功支持下单，需添加订单列表查询，判断是否添加成功

--- APIdemo/run.py
+++ APIdemo/run.py
@@ -1,8 +1,9 @@
 from httprunner.api import HttpRunner
 from httprunner.report import gen_html_report
 
-runner = HttpRunner(log_level="DEBUG")
-# summary = runner.run("testcases/verify_code_login.yml",dot_env_path="conf/.env")
-summary = runner.run("api/mall/back-end/car-item/GET_tree.yml",dot_env_path="conf/.env")
+# runner = HttpRunner(log_level="DEBUG")
+runner = HttpRunner()
+summary = runner.run("testcases/verify_code_login.yml",dot_env_path="conf/.env")
+# summary = runner.run("api/mall/back-end/car-lease-plans/item/GET_item.yml",dot_env_path="conf/.env")
 gen_html_report(summary)
 
