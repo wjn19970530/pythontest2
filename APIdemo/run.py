ebc5538c4100ca325981ec83e416640455f1783e
wjn19970530 <784628015@qq.com>
2020-08-25 15:37:14 +0800

    获取商品相关skuId

--- APIdemo/run.py
+++ APIdemo/run.py
@@ -1,8 +1,8 @@
 from httprunner.api import HttpRunner
 from httprunner.report import gen_html_report
 
-runner = HttpRunner()
+runner = HttpRunner(log_level="DEBUG")
 # summary = runner.run("testcases/verify_code_login.yml",dot_env_path="conf/.env")
-summary = runner.run("api/mall/back-end/GET_current.yml",dot_env_path="conf/.env")
+summary = runner.run("api/mall/back-end/car-item/GET_tree.yml",dot_env_path="conf/.env")
 gen_html_report(summary)
 
