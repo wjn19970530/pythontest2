8f240e81afb47a88c0b0cf5e4cfd663da03221b5
wjn19970530 <784628015@qq.com>
2020-08-26 18:54:32 +0800

    添加订单下单成功校验

--- APIdemo/run.py
+++ APIdemo/run.py
@@ -4,6 +4,6 @@ from httprunner.report import gen_html_report
 # runner = HttpRunner(log_level="DEBUG")
 runner = HttpRunner()
 summary = runner.run("testcases/verify_code_login.yml",dot_env_path="conf/.env")
-# summary = runner.run("api/mall/back-end/order/POST_paramsID.yml",dot_env_path="conf/.env")
+# summary = runner.run("api/mall/back-end/car-item/GET_tree.yml",dot_env_path="conf/.env")
 gen_html_report(summary)
 
