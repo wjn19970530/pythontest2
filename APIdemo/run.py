3eb753be21ff330f32afd865fb79aa996048e6ac
wjn19970530 <784628015@qq.com>
2020-08-28 13:34:44 +0800

    testsuit

--- APIdemo/run.py
+++ APIdemo/run.py
@@ -3,7 +3,8 @@ from httprunner.report import gen_html_report
 
 # runner = HttpRunner(log_level="DEBUG")
 runner = HttpRunner()
-summary = runner.run("testcases/verify_code_login.yml",dot_env_path="conf/.env")
-# summary = runner.run("api/mall/back-end/car-item/GET_tree.yml",dot_env_path="conf/.env")
+# summary = runner.run("testcases/verify_code_login.yml",dot_env_path="conf/.env")
+# summary = runner.run("api/auth/POST_login.yml",dot_env_path="conf/.env")
+summary = runner.run("testsuites/order.yml",dot_env_path="conf/.env")
 gen_html_report(summary)
 
