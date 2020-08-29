54fea1f9bcffa64baeee628a5680d17eb7b15eb1
wjn19970530 <784628015@qq.com>
2020-08-29 17:03:46 +0800

    testsuit

--- APIdemo/debugtalk.py
+++ APIdemo/debugtalk.py
@@ -1,5 +1,14 @@
+import os
 
-import time
-
-def sleep(n_secs):
-    time.sleep(n_secs)
+def get_token_write(response):
+    response = response.json["access_token"]
+    basc_path = os.path.dirname(os.path.realpath(__file__))
+    txt_path = os.path.join(basc_path,"data/token.txt")
+    with open(txt_path, 'w',encoding='utf-8') as f:
+        f.write(response)
+def get_token_use():
+    basc_path = os.path.dirname(os.path.realpath(__file__))
+    txt_path = os.path.join(basc_path,"data/token.txt")
+    with open(txt_path, 'r', ) as f:
+        lines = f.readline()
+        return lines
